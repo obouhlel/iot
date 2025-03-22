4214b62e5fe51fea68a2f7c85d82fc5a62bf7fea
Oussama BOUHLEL <oussama_bouhlel@outlook.fr>
2025-03-22 11:46:13 +0100

    Update setup

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-# Vérifier si Docker est en cours d'exécution
-if ! docker info > /dev/null 2>&1; then
-  echo "Docker n'est pas en cours d'exécution."
-  exit 1
-fi
-
 # Créer un cluster K3d
 echo "Création du cluster K3d..."
 k3d cluster create iot-cluster -p "8090:80@loadbalancer" -p "8043:443@loadbalancer" --agents 1
-
-# Vérifier que le cluster fonctionne
-echo "Vérification du cluster..."
-kubectl get nodes
 
 # Créer les namespaces
 echo "Création des namespaces..."
