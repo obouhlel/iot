ebc07f6e658453aa4d475bb9a2a57c0d23b85860
Oussama BOUHLEL <obouhlel@bess-f2r5s1.clusters.42paris.fr>
2025-04-21 10:46:03 +0200

    Update the etc hosts in vm

--- p2/scripts/setup.sh
+++ p2/scripts/setup.sh
@@ -9,6 +9,13 @@ mkdir -p $HOME/.kube
 sudo cp /etc/rancher/k3s/k3s.yaml $HOME/.kube/config
 sudo chown $(whoami):$(whoami) $HOME/.kube/config
 
+if ! grep -q "192.168.56.110 app1.com app2.com" /etc/hosts; then
+    echo "192.168.56.110 app1.com app2.com" | sudo tee -a /etc/hosts
+    echo "/etc/hosts updated with app1.com and app2.com"
+else
+    echo "/etc/hosts already contains app1.com and app2.com"
+fi
+
 if kubectl get nodes &> /dev/null; then
     echo "K3s Server setup complete and kubectl is working correctly!"
 else
