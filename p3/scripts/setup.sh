89661dce7908d536492eb9492a914d7280dcddaa
Oussama BOUHLEL <obouhlel@bess-f2r5s1.clusters.42paris.fr>
2025-04-22 11:58:45 +0200

    Update nginx setup

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -15,13 +15,7 @@ kubectl create namespace ingress-nginx
 kubectl create namespace dev
 
 echo "Install NGINX"
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.2/deploy/static/provider/cloud/deploy.yaml
-
-# Attendre que les pods soient prêts
-kubectl wait --namespace ingress-nginx \
-            --for=condition=ready pod \
-            --selector=app.kubernetes.io/component=controller \
-            --timeout=120s
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.12.1/deploy/static/provider/cloud/deploy.yaml
 
 # Installer Argo CD
 echo "Installation d'Argo CD..."
